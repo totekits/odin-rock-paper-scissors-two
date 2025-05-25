c95f20e0969aa8dd414cb2ef69a62a5fdcea3651
totekits <144774991+totekits@users.noreply.github.com>
2025-05-25 10:11:06 +0700

    create getHumanChoice()

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-/* create function getComputerChoice
+/* create getComputerChoice()
 to return rock, paper or scissors, 
-using Math.random */
+using Math.random() */
 function getComputerChoice() {
-  let choice = Math.random();
-  if (choice <= 0.33) {
+  let computerChoice = Math.random();
+  if (computerChoice <= 0.33) {
     return `rock`;
-  } else if (choice <= 0.66) {
+  } else if (computerChoice <= 0.66) {
     return `paper`;
   } else {
     return `scissors`;
   }
 }
 
-console.log(getComputerChoice());
+/* create getHumanChoice()
+using prompt() */
+function getHumanChoice() {
+  let humanChoice = prompt(`type rock, paper or scissors`)
+  if (humanChoice === null) {
+    return null;
+  }
+  if (humanChoice === `rock` || humanChoice === `paper` || humanChoice === `scissors`) {
+    return humanChoice;
+  } else { 
+    alert(`wrong input, type again`);
+    return getHumanChoice();
+  }
+}
+
+console.log(getHumanChoice());
