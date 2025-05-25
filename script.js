d491e4a012ee69390878b8c4c352261167a2c191
totekits <144774991+totekits@users.noreply.github.com>
2025-05-25 09:41:36 +0700

    create getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log(`hello world`)
+/* create function getComputerChoice
+to return rock, paper or scissors, 
+using Math.random */
+function getComputerChoice() {
+  let choice = Math.random();
+  if (choice <= 0.33) {
+    return `rock`;
+  } else if (choice <= 0.66) {
+    return `paper`;
+  } else {
+    return `scissors`;
+  }
+}
+
+console.log(getComputerChoice());
